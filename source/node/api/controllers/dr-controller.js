204dc99fc8506a7e31a1e9950b88d050719eee2a
shaiendra <sshail@sapient.com>
2018-01-20 10:21:02 +0000

    adding node api

---
+++ source/node/api/controllers/dr-controller.js
@@ -0,0 +1,72 @@
+'use strict';
+var mongoose = require('mongoose'),
+    Dr = mongoose.model('dr');
+
+exports.list_all_dr = function(req, res) {
+    console.log("get call for all doctor");
+    Dr.find({}, function(err, result) {
+        if (err) {
+            res.send(err);
+        }
+        res.json(result);
+    });
+};
+exports.find_dr = function(req, res) {
+    console.log("find Docutor ");
+    var name=req.body.name;
+    var regEx='.*'+name+'.*';
+    Dr.find({"name": new RegExp(regEx, 'gi')}, function(err, result) {
+        if (err) {
+            res.send(err);
+        }
+
+        res.json(result);
+    });
+};
+exports.get_dr = function(req, res) {
+    console.log("get call for single doctor " + req.params.drId);
+    Dr.find({drId:req.params.drId}, function(err, result) {
+        if (err) {
+            res.send(err);
+        }
+
+        res.json(result);
+    });
+};
+exports.insert_new_dr = function(req, res) {
+    var dr = new Dr(req.body);
+    dr.last_updated = new Date();
+    dr.save(function(err) {
+        if (err) {
+            res.send(err);
+        }
+        res.json({ message: 'data save' });
+    })
+}
+
+exports.update_a_dr = function(req, res) {
+    req.body.last_updated=new Date();
+    req.body.itemId=req.params.itemId;
+    Dr.findOneAndUpdate({itemId:req.params.itemId}, req.body, {new: true}, function(err, item) {
+        if (err)
+            res.send(err);
+        res.json({ message: "name updated" });
+    });
+
+    console.log("put call with requestId in path")
+};
+
+exports.delete_a_dr = function(req, res) {
+    Dr.remove({
+        itemId: req.params.taskId
+    }, function(err, Dr) {
+        if (err) {
+            res.send(err);
+        }
+        res.json({ message: "Successful delete" });
+    });
+    console.log("delete call withe requestId in path")
+};
+/**
+ * Created by sshail on 03/06/2017.
+ */
---
+++ source/node/api/routes/app-routes.js
@@ -0,0 +1,35 @@
+/**
+ * Created by sshail on 03/06/2017.
+ */
+'use strict';
+
+module.exports = function(app) {
+    var userController = require('../controllers/user-controller.js');
+    var drController = require('../controllers/dr-controller.js');
+	/*
+    var locationController = require('../controllers/location-controller.js');
+	*/
+
+    app.route('/user')
+        .get(userController.list_all_users)
+        .post(userController.create_new_user)
+        .put(userController.update_user)
+        .delete(userController.invalidate_user);
+
+    app.route('/user/:userId')
+        .get(userController.get_users);
+
+    app.route('/dr')
+        .get(drController.list_all_dr)
+        .post(drController.insert_new_dr);
+
+    app.route('/dr/search')
+        .post(drController.find_dr);
+
+    app.route('/dr/:drId')
+        .get(drController.get_dr)
+        .put(drController.update_a_dr)
+        .delete(drController.delete_a_dr);
+    
+    
+};
